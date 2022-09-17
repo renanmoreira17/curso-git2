6c68905f9e743f03c1998097faa0304d2f65f1f0
Renan Moreira <renanmoreira17@gmail.com>
2022-09-17 15:26:43 -0300

    Update main.c

--- main.c
+++ main.c
@@ -26,10 +26,13 @@ int div(int a, int b)
 int main() {
     int n1, n2, n3;
     n3 = add(n1, n2);
+
     float f1, f2, f3;
     f3 = add_f(f1, f2);
+
     int m1, m2, m3;
     m3 = mult(m1, m2);
+
     int d1, d2, d3;
     d3 = div(d1, d2);
 
